2e50be9fc3943b8539db3953482a350967a8b0eb
isaki001 <Ioannis.Sakiotis@amd.com>
2025-04-10 19:26:54 -0500

    remove prints

--- cmake/MSCCLPP.cmake
+++ cmake/MSCCLPP.cmake
@@ -98,6 +98,11 @@ if(ENABLE_MSCCLPP)
             WORKING_DIRECTORY ${MSCCLPP_SOURCE}
         )
 
+        execute_process(
+            COMMAND git apply ${CMAKE_CURRENT_SOURCE_DIR}/ext-src/remove_prints.patch
+            WORKING_DIRECTORY ${MSCCLPP_SOURCE}
+        )
+
         set(CMAKE_INHERITED_ARGS "")
         set(CMAKE_ARGS_LIST "CMAKE_PREFIX_PATH;CMAKE_INSTALL_RPATH_USE_LINK_PATH;HIP_COMPILER")
         foreach(arg IN LISTS CMAKE_ARGS_LIST)
@@ -140,6 +145,11 @@ if(ENABLE_MSCCLPP)
         )
 
         find_package(mscclpp_nccl REQUIRED)
+        execute_process(
+            COMMAND git apply --reverse ${CMAKE_CURRENT_SOURCE_DIR}/ext-src/remove_prints.patch
+            WORKING_DIRECTORY ${MSCCLPP_SOURCE}
+        )
+
 
     execute_process(
             COMMAND git apply --reverse ${CMAKE_CURRENT_SOURCE_DIR}/ext-src/graph_mode_read_flag.patch
